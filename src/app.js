6a8f70d5565bcba8aeefa55e0ad51cbfda00650b
Ayushya <jaiswalayushya@gmail.com>
2024-05-17 20:15:27 +0530

    Initialized email management service

---
+++ src/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import { errorHandler } from 'lib-finance-service';
+
+import { EMAIL_API } from './constants.js';
+
+// Email Routes
+
+const app = express();
+
+// Setting up Middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // reflecting the request origin
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: '64kb' // Maximum request body size.
+}));
+
+app.use(express.urlencoded({
+    limit: '32kb',
+    extended: false
+}));
+
+// Email Route
+
+// Error Handler Middleware
+app.use(errorHandler);
+
+export default app;
