82f8a66f531ffe11318d8bfddad810b3d651a30a
Ayushya <jaiswalayushya@gmail.com>
2024-05-19 16:31:40 +0530

    Added verification successful mail template to emails svc

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const mailTemplateName = {
-    USER_VERIFICATION_MAIL: 'userVerificationMail'
+    USER_VERIFICATION_MAIL: 'userVerificationMail',
+    VERIFICATION_SUCCESSFULL_MAIL: 'verificationSuccessfulMail'
 }
 
 const mailSubjectLine = {
-    USER_VERIFICATION_MAIL: 'Welcome to Finance Tracker - Verify Your Email and Activate Your Account.'
+    USER_VERIFICATION_MAIL: 'Welcome to Finance Tracker - Verify Your Email and Activate Your Account.',
+    VERIFICATION_SUCCESSFULL_MAIL: 'Account Verified - Welcome to Finance Tracker.'
 }
 
 export {
