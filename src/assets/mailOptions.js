e3ad11b13ab6bc05521f780ea59f5cbc4d6f276f
Ayushya <jaiswalayushya@gmail.com>
2024-05-27 10:06:19 +0530

    Added userUpdateDetailsMail template

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -3,13 +3,15 @@
 const mailTemplateName = {
     USER_VERIFICATION_MAIL: 'userVerificationMail',
     VERIFICATION_SUCCESSFULL_MAIL: 'verificationSuccessfulMail',
-    USER_REACTIVATE_MAIL: 'accountReactivateMail'
+    USER_REACTIVATE_MAIL: 'accountReactivateMail',
+    USER_UPDATED_MAIL: 'userUpdatedMail'
 }
 
 const mailSubjectLine = {
     USER_VERIFICATION_MAIL: 'Welcome to Finance Tracker - Verify Your Email and Activate Your Account.',
     VERIFICATION_SUCCESSFULL_MAIL: 'Account Verified - Welcome to Finance Tracker.',
-    USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.'
+    USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.',
+    USER_UPDATED_MAIL: 'Account Details Successfully Updated'
 }
 
 export {
